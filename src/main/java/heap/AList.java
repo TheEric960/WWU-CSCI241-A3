16bf3ff0198ce3a542de711864be3d3b2a76e84e
Jack Wilson <wilso313@wwu.edu>
2019-02-27 19:55:42 -0800

    copied lab 5 over

--- src/main/java/heap/AList.java
+++ src/main/java/heap/AList.java
@@ -1,10 +1,9 @@
 package heap;
 
-import java.lang.reflect.Array;
 import java.util.NoSuchElementException;
 
 /** An ArrayList-like dynamic array class that allocates
-* new memory when needed */
+ * new memory when needed */
 public class AList<T> {
 
   protected int size; // number of elements in the AList
@@ -26,53 +25,66 @@ public class AList<T> {
 
   /** Creates an AList with the given capacity */
   public AList(int capacity) {
-    // TODO 1
+    a = createArray(capacity);
+    size = 0;
   }
 
   /* Grows a to double its current size if newSize exceeds a's capacity. Does
-  * nothing if newSize < a.length.  Grow the array by allocating a new array
-  * and copying the old array's contents into the new one. This does *not*
-  * change the AList's size. */
+   * nothing if newSize < a.length.  Grow the array by allocating a new array
+   * and copying the old array's contents into the new one. This does *not*
+   * change the AList's size. */
   protected void growIfNeeded(int newSize) {
-    // TODO 2a
+    while (newSize > a.length) {
+      size = a.length * 2;
+      T[] tmp = createArray(size);
+      // make sure the new array can support the newSize, else expand more
+      if (tmp.length >= newSize) {
+        System.arraycopy(a, 0, tmp, 0, a.length);
+        a = tmp;
+      }
+    }
   }
 
   /** Resizes the AList.
-  *  this *does* modify the size, and may modify the capacity if newsize
-  *  exceeds capacity. */
+   *  this *does* modify the size, and may modify the capacity if newsize
+   *  exceeds capacity. */
   public void resize(int newsize) {
-    // TODO 2b
+    growIfNeeded(newsize);
+    size = newsize;
   }
 
   /** Gets element i from AList.
-  * @throws ArrayIndexOutOfBoundsException if 0 <= i < size does not hold */
+   * @throws ArrayIndexOutOfBoundsException if 0 <= i < size does not hold */
   public T get(int i) {
-    // TODO 3a
-    throw new UnsupportedOperationException(); // delete this once implemented!
+    return a[i];
   }
 
   /** Sets the ith element of the list to value.
-  * @throws ArrayIndexOutOfBoundsException if 0 <= i < size does not hold */
+   * @throws ArrayIndexOutOfBoundsException if 0 <= i < size does not hold */
   public void put(int i, T value) {
-    // TODO 3b
+    a[i] = value;
   }
 
   /** Appends value at the end of the AList, increasing size by 1.
-  * Grows the array if needed to fit the appended value */
+   * Grows the array if needed to fit the appended value */
   public void append(T value) {
-    // TODO 4a
+    int tmp = size;
+    growIfNeeded(size + 1);
+    size = tmp; // size modified in growIfNeeded, must reset
+    a[size] = value;
+    size++;
   }
 
   /** Removes and returns the value at the end of the AList.
-  *  this *does* modify size and cannot modify capacity.
-  *  @throws NoSuchElementException if size == 0*/
+   *  this *does* modify size and cannot modify capacity.
+   *  @throws NoSuchElementException if size == 0*/
   public T pop() {
-    // TODO 4b
-    throw new UnsupportedOperationException(); // delete this once implemented!
+    size--;
+    return a[size];
   }
 
   /*  Create and return a T[] of size n.
-  *  This is necessary because generics and arrays don't play well together.*/
+   *  This is necessary because generics and arrays don't play well together.*/
   @SuppressWarnings("unchecked")
   protected T[] createArray(int size) {
     return (T[]) new Object[size];
